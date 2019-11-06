a3a81f67fd17a9613f2dbb8cd347d0ccb4655280
Ana Maria Antoaneta Tiru <Ana.Tiru@insse.ro>
2019-11-06 16:47:51 +0200

    fin

--- suprafata.R
+++ suprafata.R
@@ -26,16 +26,34 @@ search_send <- rmdSel$findElement(using  = "css", value = "button.sb-searchbox__
 search_send$clickElement()
 Sys.sleep(2)
 
+
 # Click hotel
+hot <- read_html(rmdSel$getPageSource()[[1]])  
+lin <- hot %>% html_nodes(css = "div.nodates_hotels>div:nth-child(2)>div>div>div>div>h3>a") %>% html_attr("href")
+lin <- gsub("\n", "", lin)
+lin <- paste0("https://www.booking.com", lin)
+
+rmdSel$open()
+rmdSel$navigate(lin)
+
+# Click camera
+click_camera <- rmdSel$findElement(using  = "css", value = "div.bodyconstraint_increased-min-width>div>div>div>div.rlt-right>div.hotelchars>div>div>div.description>table>tbody>tr.odd:first-child>td>div>div>a.jqrt>i.rt_room_type_ico")
+click_camera$clickElement()
+
+supr <- read_html(rmdSel$getPageSource()[[1]])
+x <- supr %>% html_nodes(css = "div.room-lightbox-container>div>div.hprt-lightbox-right-container") %>% html_text()
+x <- gsub("\n", "", x)
+x <- gsub("\t", "", x)
+x <- gsub(" ", "", x)
+
+gsub("m².*","",x)
+
+
+gsub("[^[:digit:].]", "",  x)
+
+str_extract(x, "[[:digit:]]+")
+
+as.numeric(gsub("[^\\d]+", "", x, perl=TRUE))
 click_hot <- rmdSel$findElement(using  = "css", value = "div>div>div>div>div>h3>a>span")
 click_hot$clickElement()
 Sys.sleep(2)
-
-# Click camera
-click_camera <- rmdSel$findElement(using  = "css", value = "div.bodyconstraint_increased-min-width>div>div>div>div.rlt-right>div.hotelchars>div>div>div.description>table>tbody>tr.odd:first-child>td>div>div>a.jqrt")
-
-rmdSel$findElement(using  = "css", value ="div.room-info")
-click_camera$clickElement()
-Sys.sleep(2)
-
-"td.ftd>div>div.room-info>a"
